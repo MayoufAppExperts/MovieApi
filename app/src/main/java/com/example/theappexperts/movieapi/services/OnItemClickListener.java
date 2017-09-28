3d09544359827fe70752827f3c7b50415ec5583b
MayoufAppExperts <mmayouf@outlook.com>
2017-09-28 16:30:41 +0100

    update

---
+++ app/src/main/java/com/example/theappexperts/movieapi/services/OnItemClickListener.java
@@ -0,0 +1,8 @@
+package com.example.theappexperts.movieapi.services;
+
+import com.example.theappexperts.movieapi.movieDetails.MoviesResponse;
+
+public interface OnItemClickListener {
+
+    void onItemClick(MoviesResponse moviesResponse);
+}
